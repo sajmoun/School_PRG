1d0a946bdf44f6e6e629fa7dd7d95766dda06b21
sajmoun <149604607+sajmoun@users.noreply.github.com>
2024-02-14 14:14:57 +0100

    Update linked_list.py

--- magic-main/magic-main/task3/linked_list.py
+++ magic-main/magic-main/task3/linked_list.py
@@ -11,3 +11,52 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
         self.length = 0
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next_node = None
+        self.prev_node = None
+
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next_node = None
+        self.prev_node = None
+
+#######################
+class DoublyLinkedList:
+    def __init__(self):
+        super().__init__()
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            new_node.prev_node = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index:int):
+        if not isinstance(index, int):
+            raise TypeError
+        node = self.head
+        if index < 0:
+            node = self.tail
+            for i in range(abs(index)-1):
+                node = node.prev_node
+            return node.data
+        else:
+            for i in range(index):
+                node = node.next_node
+            return node.data
